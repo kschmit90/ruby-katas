9ba16b23b23296c363f5b68c2f45752865e6db9f
“Kyle <kschmit90@gmail.com>
2014-08-26 20:02:08 -0500

    Wrote method to split names wrote method to add names to hash tom doc on everything

--- day2/gift_exchange.rb
+++ day2/gift_exchange.rb
@@ -1,13 +1,46 @@
 require 'pry'
-
+# Gift Exchange Module
+#
+# Used to split up members of a gift exchange and assign them to each other for gift giving.
 module GiftExchange
   
+  # Sample list of members for gift exchange.
+  #
+  # Returns an Array of member names.
   def self.sample_members
     ["Sumeet Jain", "Beth Haubert", "Sachin Jain", "Suneel Jain", "Prisha Gupta", "Richa Goyal", "Arjun Goyal", "Judy Haubert"]
   end
   
-  def self.assign_gift_givers(members)
+  # Split Members into first and last names
+  #
+  # members - Array of members
+  #
+  # Returns an Array of Arrays containing first and last names.
+  def self.split_members(members)
+    split_names = []
     
+    members.each do |x|
+      split_names << x.split(' ')
+    end
+    split_names
+  end
+    
+  # Take split member names as an Array of Arrays and associate them via key value in Hash
+  #
+  # split_names - Array of Arrays of first and last names
+  #
+  # Returns a Hash of first name keys and last name values 
+  def self.split_members_hash(split_names)
+    members_hash = {}
+    
+    split_names.each do |arr|
+      members_hash[arr[0]] = arr[1]
+    end
+    members_hash
+  end
+    
+  def self.assign_gift_givers(members)
+      
   end
 end
 
--- day2/spec/gift_exchange_spec.rb
+++ day2/spec/gift_exchange_spec.rb
@@ -2,4 +2,23 @@ require_relative '../gift_exchange.rb'
 
 describe GiftExchange do
 
+  it 'should return an array of names' do
+    expect(GiftExchance.sample_members).to eql(["Sumeet Jain", "Beth Haubert", "Sachin Jain", "Suneel Jain", "Prisha Gupta", "Richa Goyal", "Arjun Goyal", "Judy Haubert"])
+  end
+  
+  it 'should return a hash of assigned members' do
+    members = ["Sumeet Jain", "Beth Haubert", "Sachin Jain", "Suneel Jain", "Prisha Gupta", "Richa Goyal", "Arjun Goyal", "Judy Haubert"]
+
+    expect(GiftExchange.assign_gift_givers(members).class).to eql(Hash)
+  end
+  
+  it 'should only assign two members with different last names' do
+  end
+  
+  it 'should ensure all members are assigned' do
+  end
+  
+  it 'should properly determine a members last name' do
+  end
+
 end
